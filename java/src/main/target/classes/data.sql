25d1a6672f172e5cd1832f1e35cb8aa26dfcbff5
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-08-10 20:22:54 -0400

    Add files via upload

---
+++ java/src/main/target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tbl_expenses (expense, description, amount) VALUES
+    ('Burger King', 'Went out with Damian late night and grabbed BK through drive-thru', 20.80),
+    ('PSN Subscription', 'Renewed my yearly PSN subscription', 65.07),
+    ('Louis Vuitton bag', 'Thank you gift for mom', 1708.54),
+    ('Flight to Savannah GA', 'Went to visit family friend', 854.98);
